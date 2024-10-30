efbe9fd7480605e0d257425b1daf664fddaf5b22
Gregory mulvaney <gregtmulvaney@gmail.com>
2024-10-30 11:05:58 -0500

    Fixed min dimensions

--- table/table.go
+++ table/table.go
@@ -1,15 +1,17 @@
 package table
 
 import (
+	"fmt"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 type Model struct {
-	cols       []Column
-	rows       []Row
-	dimensions dimensions
-	styles     Styles
+	cols []Column
+	rows []Row
+
+	dimensions
 }
 
 type Row struct {
@@ -19,8 +21,8 @@ type Row struct {
 
 type Column struct {
 	Title string
-	Width int
-	Flex  bool
+	Width *int
+	Flex  *bool
 }
 
 type Styles struct {
@@ -36,7 +38,8 @@ type dimensions struct {
 
 type Option func(*Model)
 
-func New(opts ...Option) (m Model) {
+func New(opts ...Option) Model {
+	m := Model{}
 
 	for _, opt := range opts {
 		opt(&m)
@@ -50,67 +53,64 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
+
 	return m, nil
 }
 
 func (m Model) View() string {
-	header := m.renderHeader()
-	return lipgloss.JoinVertical(lipgloss.Top, header)
-}
-
-func (m *Model) renderHeaderColumns() []string {
-	renderedColumns := make([]string, len(m.cols))
-	// Width of table consumed by static columns
-	populatedWidth := 0
-	// Count of columns with Flex enabled
-	flexColumnCount := 0
-
-	for i, column := range m.cols {
-		if column.Flex {
-			flexColumnCount += 1
-			continue
-		}
-
-		if column.Width != 0 {
-			renderedColumns[i] = lipgloss.NewStyle().Width(column.Width).MaxWidth(column.Width).Render(column.Title)
-			populatedWidth += column.Width
-		}
-	}
-
-	availableWidth := m.dimensions.width - populatedWidth
-	flexCellWidth := availableWidth / flexColumnCount
-	for i, column := range m.cols {
-		if column.Flex == false {
-			continue
-		}
-
-		renderedColumns[i] += lipgloss.NewStyle().Width(flexCellWidth).MaxWidth(flexCellWidth).Render(column.Title)
-	}
-
-	return renderedColumns
-}
-
-func (m *Model) renderHeader() string {
-	headerColumns := m.renderHeaderColumns()
-	header := lipgloss.JoinHorizontal(lipgloss.Left, headerColumns...)
-	return lipgloss.NewStyle().Width(m.dimensions.width).MaxWidth(m.dimensions.width).Render(header)
+	return fmt.Sprintf("H:%d W%d", m.dimensions.height, m.dimensions.width)
 }
 
 func (m *Model) UpdateDimensions(width int, height int) {
-	m.dimensions = dimensions{
-		width:  width,
-		height: height,
-	}
+
 }
 
-func WithColumns(cols []Column) Option {
-	return func(m *Model) {
-		m.cols = cols
-	}
-}
+// func (m *Model) renderHeaderColumns() []string {
+// 	renderedColumns := make([]string, len(m.cols))
+// 	// Width of table consumed by static columns
+// 	populatedWidth := 0
+// 	// Count of columns with Flex enabled
+// 	flexColumnCount := 0
 
-func WithRows(rows []Row) Option {
-	return func(m *Model) {
-		m.rows = rows
-	}
-}
+// 	for i, column := range m.cols {
+// 		if column.Flex {
+// 			flexColumnCount += 1
+// 			continue
+// 		}
+
+// 		if column.Width != 0 {
+// 			renderedColumns[i] = lipgloss.NewStyle().Width(column.Width).MaxWidth(column.Width).Render(column.Title)
+// 			populatedWidth += column.Width
+// 		}
+// 	}
+
+// 	availableWidth := m.dimensions.width - populatedWidth
+// 	flexCellWidth := availableWidth / flexColumnCount
+// 	for i, column := range m.cols {
+// 		if column.Flex == false {
+// 			continue
+// 		}
+
+// 		renderedColumns[i] += lipgloss.NewStyle().Width(flexCellWidth).MaxWidth(flexCellWidth).Render(column.Title)
+// 	}
+
+// 	return renderedColumns
+// }
+
+// func (m *Model) renderHeader() string {
+// 	headerColumns := m.renderHeaderColumns()
+// 	header := lipgloss.JoinHorizontal(lipgloss.Left, headerColumns...)
+// 	return lipgloss.NewStyle().Width(m.dimensions.width).MaxWidth(m.dimensions.width).Render(header)
+// }
+
+// func WithColumns(cols []Column) Option {
+// 	return func(m *Model) {
+// 		m.cols = cols
+// 	}
+// }
+
+// func WithRows(rows []Row) Option {
+// 	return func(m *Model) {
+// 		m.rows = rows
+// 	}
+// }
